9435f3a20b5967edff451ad4b9998b23eeeff473
BasilKhouli <basil.khouli@gmail.com>
2024-02-02 21:25:13 +1300

    To do list comments

--- src/main/kotlin/me/basil/xraces/events/LightLevelChecker.kt
+++ src/main/kotlin/me/basil/xraces/events/LightLevelChecker.kt
@@ -2,3 +2,4 @@ package me.basil.xraces.events
 
 class LightLevelChecker {
 }
+// Check light level of player location to apply custom attributes based on light.
---
+++ src/main/kotlin/me/basil/xraces/utils/PlayerLocation.java
@@ -0,0 +1,6 @@
+package me.basil.xraces.utils;
+
+public class PlayerLocation {
+}
+
+//Check player location (outside, inside, etc.) to apply attributes/effects based on location
